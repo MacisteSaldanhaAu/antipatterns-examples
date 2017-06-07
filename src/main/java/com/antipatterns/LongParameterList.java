45eb6f7ac3b3d0929c29c81cfe2c9ded64caa205
Maciste Saldanha <maciste.saldanha@aurea.com>
2017-06-07 13:02:40 -0300

    First commit

---
+++ src/main/java/com/antipatterns/LongParameterList.java
@@ -0,0 +1,20 @@
+package com.antipatterns;
+
+public class LongParameterList {
+
+    void method1(int a, int b, int c, int d, int e, int f) {
+        return;
+    }
+
+    void method2(int a, int b, int c, int d, int e) {
+        return;
+    }
+
+    void methodWithLongParameter1(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k) {
+        return;
+    }
+
+    void methodWithLongParameter2(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k) {
+        return;
+    }
+}
